b6f03c1bd175add8ab298e43119cf50f83d00ea9
M. Usman Maulana <um59411@gmail.com>
2023-05-16 20:51:56 +0700

    libray sederhana composer hello:

---
+++ src/Person.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Usman\Hello;
+
+class Person
+{
+    function __construct(private string $name)
+    {
+    }
+
+    function hello(string $name): string
+    {
+        return "Hello $name, my name is $this->name" . PHP_EOL;
+    }
+}
